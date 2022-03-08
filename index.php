8e126de986ea3fc8c422841bbcbfbc6408a26493
Maadelka <dikarizal62@gmail.com>
2022-03-09 00:13:23 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php 
+
+
+
+// ===========================html
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+    <title>example</title>
+</head>
+<body>
+
+<input type="range" value="0" min="0" max="500" step="1" oninput="myFunction(this.value)" />
+<span id="hasil1">0</span>
+
+<div class="kotak"></div>
+
+
+
+<script src="js/script.js"></script>
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,4 @@
+function myFunction(myValue){
+    document.getElementById("hasil1").innerHTML = myValue;
+}
+
